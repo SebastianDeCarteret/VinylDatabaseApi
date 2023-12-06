7b086dd2b485c5d94b1e1d09384e0a9e2a060014
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-12-06 10:10:46 +0000

    no actual changes

---
+++ Migrations/20231206100017_UpdateVinylToNullable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VinylDatabaseApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateVinylToNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Vinyl",
+                columns: new[] { "VinylId", "Artist", "ImageLink", "NumberOfLps", "NumberOfTracks", "Price", "ReleaseDate", "Title" },
+                values: new object[] { 1, "Bon Jovi", "Link", 2, 16, 39.99f, new DateTime(1982, 3, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "Slippery When Wet" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Vinyl",
+                keyColumn: "VinylId",
+                keyValue: 1);
+        }
+    }
+}
